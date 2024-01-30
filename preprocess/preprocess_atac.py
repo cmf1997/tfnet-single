68b695876b951e9ef41cb660daf7e0c8ce54f0fa
cmf1997 <562590731@qq.com>
2024-01-30 19:19:28 +0800

    cell line atac

---
+++ preprocess/preprocess_atac.py
@@ -0,0 +1,27 @@
+# cell line atac seq not perturbation
+
+# for HepG2
+ENCSR042AWH, GRCh38
+# for K562
+ENCSR483RKN, GRCh38
+# for HEK293
+https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE108513
+# for GM12878
+ENCSR095QNB, GRCh38
+# for MCF-7
+ENCSR422SUG, GRCh38
+# for WTC11
+ENCSR541KFY, GRCh38
+# for A549 
+ENCSR032RGS, GRCh38
+ENCSR220ASC, GRCh38, single end
+# for SK-N-SH
+ENCSR587TRP, GRCh38
+
+
+# for HeLa-S3 # note: cell cycle
+https://www.ncbi.xyz/geo/query/acc.cgi?acc=GSM5848501
+https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE195693
+
+# for H1
+https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE85330  # day 0 
